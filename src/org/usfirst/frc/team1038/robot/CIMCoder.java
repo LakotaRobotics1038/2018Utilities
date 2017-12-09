4a913dfea57db944d6ced39617e7dda3dacd6207
Parshva Shah <pkshah2017@gmail.com>
2017-12-09 11:19:51 -0500

    Added CIMCoder.java

---
+++ src/org/usfirst/frc/team1038/robot/CIMCoder.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team1038.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+
+public class CIMCoder {
+	
+	Encoder encoder;
+	
+	public CIMCoder(int channelA, int channelB, boolean isInverted, int countsPerRevolution, double wheelDiameter)
+	{
+		encoder = new Encoder(channelA, channelB, isInverted);
+		encoder.setDistancePerPulse(findDistancePerPulse(countsPerRevolution, wheelDiameter));
+	}
+	
+	public int getCount()
+	{
+		return encoder.get();
+	}
+	
+	public double getRate()
+	{
+		return encoder.getRate();
+	}
+	
+	private double findDistancePerPulse(int countsPerRevolution, double wheelDiameter)
+	{		
+		return 1/(countsPerRevolution * wheelDiameter * Math.PI);
+	}
+}
